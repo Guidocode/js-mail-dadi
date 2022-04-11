b68d9adde75f0a48dd4134f73abeeaab9a89da1b
Guidocode <guidobuono@icloud.com>
2022-04-11 21:08:46 +0200

    dadi

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 
+// MAIL 
+
 // Creo una lista di utenti autorizzati
 const classe61 = [
   'domenicogostino@icloud.com',
@@ -25,15 +27,20 @@ const classe61 = [
 
 
 // Chiedo all'utente la sua mail 
-const mail = prompt('Inserisci la tua mail');
+// const mail = prompt('Inserisci la tua mail');
+const mail = 'guidobuono@icloud.com';
 
-let output = document.querySelector('#message-output');
+const btnTira = document.querySelector('.gb-btn');
 
-// controllo che sia nella lista di chi può accedere
+let mailOutput = document.querySelector('#mail-output');
+let dadiOutput = document.querySelector('#dadi-output');
+let arrowDown = document.querySelector('.arrow');
+
 
 // di dafault stabilisco che il mio nome non sia presnte
 let mailPresente = false;
 
+// controllo che sia nella lista di chi può accedere
 // cerco la mail digitata dentro l'array classe 61
 for(let i = 0; i < classe61.length; i++){
   // se la mail digitata è uguale a un elemento dell'array
@@ -45,9 +52,40 @@ for(let i = 0; i < classe61.length; i++){
 
 // Stampo il messaggio in base al risultato
 if(mailPresente){
-  output.innerHTML = 'Sei presente nella lista! Gioca hai dadi!'
+  mailOutput.innerHTML = 'Sei presente nella lista! Gioca hai dadi!';
   console.log('trovato');
+  arrowDown.classList.add('opacity');
+  dadiOutput.innerHTML = 'Tira i dadi!'
+  btnTira.classList.add('active');
 } else {
-  output.innerHTML = 'Non sei presente nella lista!'
+  mailOutput.innerHTML = 'Non sei presente nella lista!'
   console.log('non trovato');  
+  dadiOutput.innerHTML = 'Mi spiace, non puoi giocare...'
 }
+
+
+// DADI 
+let nPc = document.querySelector('.pc');
+let nUser = document.querySelector('.user');
+let nRandom = document.querySelector('.n-random');
+let winLose = document.querySelector('.win-lose');
+
+
+btnTira.addEventListener('click', function(){
+  nRandom.classList.add('opacity');
+  nPc.innerHTML = Math.ceil(Math.random()*6);
+  nUser.innerHTML = Math.ceil(Math.random()*6);
+
+
+  console.log(nPc);
+  console.log(nUser);
+  
+  // NON FUNZIONA 
+  if(nPc > nUser){
+    winLose.innerHTML = 'Hai perso...'
+  }else if(nPc < nUser){
+    winLose.innerHTML = 'Hai vinto!'
+  }else{
+    winLose.innerHTML = 'Pareggio!'
+  }
+});
